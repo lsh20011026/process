da927dc8cd9417684f0518210d3df8bae5ab21e5
lsh20011026 <151994259+lsh20011026@users.noreply.github.com>
2024-05-30 22:14:30 +0900

    process.cpp

--- process/process.cpp
+++ process/process.cpp
@@ -7,6 +7,7 @@
 #include <algorithm>
 #include <numeric>
 #include <chrono>
+#include <queue>
 
 using namespace std;
 
@@ -14,42 +15,44 @@ mutex cout_mutex;
 
 // 전역 변수로 실행 중인 프로세스 수를 관리
 int running_processes = 0;
+queue<pair<string, bool>> command_queue; // 명령어 큐: <명령어, 백그라운드 여부>
 
 void print_prompt(const string& command) {
     lock_guard<mutex> lock(cout_mutex);
     cout << "prompt> " << command << endl;
 }
 
-void echo(const string& str, int period, int duration, int num) {
-    // Running 메시지 출력
-    {
+void echo(const string& str, int period = 0, int duration = 0, int num = 0, bool is_background = false) {
+    if (period == 0 && duration == 0 && num == 0) {
         lock_guard<mutex> lock(cout_mutex);
-        cout << "Running: [" << ++running_processes << "B]" << endl;
-        cout << "---------------------------" << endl;
+        cout << str << endl;
     }
-
-    vector<thread> threads;
-    for (int i = 0; i < num; ++i) {
-        threads.push_back(thread([str, period, duration]() {
-            int iterations = duration / period;
-            for (int t = 0; t < iterations; ++t) {
-                {
-                    lock_guard<mutex> lock(cout_mutex);
-                    cout << str << endl;
+    else {
+        vector<thread> threads;
+        for (int i = 0; i < num; ++i) {
+            threads.push_back(thread([str, period, duration, is_background]() {
+                int iterations = duration / period;
+                for (int t = 0; t < iterations; ++t) {
+                    {
+                        lock_guard<mutex> lock(cout_mutex);
+                        cout << str << endl;
+                    }
+                    this_thread::sleep_for(chrono::seconds(period));
                 }
-                this_thread::sleep_for(chrono::seconds(period));
-            }
-            }));
-    }
+                if (!is_background) {
+                    lock_guard<mutex> lock(cout_mutex);
+                }
+                }));
+        }
 
-    for (auto& t : threads) {
-        t.join();
-    }
+        for (auto& t : threads) {
+            t.join();
+        }
 
-    // 실행이 끝난 후 실행 중인 프로세스 수 감소
-    {
-        lock_guard<mutex> lock(cout_mutex);
-        --running_processes;
+        if (is_background) {
+            lock_guard<mutex> lock(cout_mutex);
+            --running_processes; // 백그라운드에서 실행되었을 경우에만 감소
+        }
     }
 }
 
@@ -65,7 +68,6 @@ void gcd_command(int x, int y) {
     lock_guard<mutex> lock(cout_mutex);
     cout << "GCD of " << x << " and " << y << " is " << gcd(x, y) << endl;
 }
-
 vector<bool> sieve(int max) {
     vector<bool> is_prime(max + 1, true);
     is_prime[0] = is_prime[1] = false;
@@ -127,20 +129,19 @@ void process_command(const string& command, bool is_background) {
     string cmd;
     iss >> cmd;
 
-    if (!is_background)
-        print_prompt(command);
+    print_prompt(command);
 
     if (is_background) {
-        // Running 메시지 출력
         {
             lock_guard<mutex> lock(cout_mutex);
-            cout << "Running background command: [" << ++running_processes << "B]" << endl;
+            cout << "Running: [" << ++running_processes << "B]" << endl;
+            cout << "---------------------------" << endl;
         }
     }
 
     if (cmd == "echo") {
         string str;
-        int period = 1, duration = 100, num = 1;
+        int period = 0, duration = 0, num = 0;
         iss >> str;
         string option;
         while (iss >> option) {
@@ -154,56 +155,20 @@ void process_command(const string& command, bool is_background) {
                 iss >> num;
             }
         }
-
-        // 백그라운드 실행이면서 출력을 보장하기 위해 추가 처리
-        if (is_background) {
-            // 먼저 기본적인 정보를 출력
-            cout << "echo " << str << " -p " << period << " -d " << duration << " -n " << num << " &" << endl;
-        }
-
-        // 명령을 실행
         echo(str, period, duration, num);
-
-        // 실행이 끝난 후 실행 중인 프로세스 수 감소
-        if (is_background) {
-            lock_guard<mutex> lock(cout_mutex);
-            --running_processes;
-            cout << "---------------------------" << endl;
-        }
     }
     else if (cmd == "dummy") {
         dummy();
-
-        // 실행이 끝난 후 실행 중인 프로세스 수 감소
-        if (is_background) {
-            lock_guard<mutex> lock(cout_mutex);
-            --running_processes;
-            cout << "---------------------------" << endl;
-        }
     }
     else if (cmd == "gcd") {
         int x, y;
         iss >> x >> y;
         gcd_command(x, y);
-
-        // 실행이 끝난 후 실행 중인 프로세스 수 감소
-        if (is_background) {
-            lock_guard<mutex> lock(cout_mutex);
-            --running_processes;
-            cout << "---------------------------" << endl;
-        }
     }
     else if (cmd == "prime") {
         int x;
         iss >> x;
         prime_command(x);
-
-        // 실행이 끝난 후 실행 중인 프로세스 수 감소
-        if (is_background) {
-            lock_guard<mutex> lock(cout_mutex);
-            --running_processes;
-            cout << "---------------------------" << endl;
-        }
     }
     else if (cmd == "sum") {
         int x, m = 1, num = 1;
@@ -218,38 +183,52 @@ void process_command(const string& command, bool is_background) {
             }
         }
         sum_command(x, m, num);
+    }
 
-        // 실행이 끝난 후 실행 중인 프로세스 수 감소
-        if (is_background) {
-            lock_guard<mutex> lock(cout_mutex);
-            --running_processes;
-            cout << "---------------------------" << endl;
-        }
+    if (is_background && cmd != "echo") { // echo는 스레드 내에서 감소
+        lock_guard<mutex> lock(cout_mutex);
+        --running_processes;
     }
 }
 
 void process_line(const string& line) {
     bool background = false;
     string command = line;
-    if (command.back() == '&') {
+
+    if (!command.empty() && command.front() == '&') {
         background = true;
-        command.pop_back(); // '&' 제거
+        command.erase(0, 1); // '&' 제거
     }
 
-    if (background) {
+    command_queue.push(make_pair(command, background));
+}
+
+void execute_command(const string& command, bool is_background) {
+    if (is_background) {
         thread(process_command, command, true).detach();
     }
     else {
         process_command(command, false);
     }
 }
-
 int main() {
     ifstream file("commands.txt");
     string line;
 
     while (getline(file, line)) {
         process_line(line);
+    }
+
+    while (!command_queue.empty()) {
+        auto cmd = command_queue.front();
+        command_queue.pop();
+        execute_command(cmd.first, cmd.second);
         this_thread::sleep_for(chrono::seconds(1)); // 각 명령어 사이에 1초 대기
     }
+
+    // 모든 백그라운드 명령어가 완료될 때까지 대기
+    while (running_processes > 0) {
+        this_thread::sleep_for(chrono::milliseconds(1));
+    }
+    return 0;
 }
